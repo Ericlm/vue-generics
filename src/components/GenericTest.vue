959483ad4db9af2af5697eff0c6c3adea3a52fce
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2023-07-21 21:18:01 +0200

    🎉 Initial commit

---
+++ src/components/GenericTest.vue
@@ -0,0 +1,9 @@
+<template>
+    <b>{{ value }}</b>
+</template>
+
+<script lang="ts" setup generic="T">
+defineProps<{
+    value: T
+}>()
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import GenericTest from './GenericTest.vue';
+
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <GenericTest value="Hello World!" random="coucou" />
+</template>
